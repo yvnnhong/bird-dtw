2781348b8996ce7b6c74a11d1b1ae89f5672f66c
Yvonne Hong <yvonnehong2003@gmail.com>
2026-07-31 11:33:25 -0700

    added constructor for core dtw file

--- src/bird_dtw/dtw.py
+++ src/bird_dtw/dtw.py
@@ -1,6 +1,6 @@
 # core DTW file. 
 class DTW: 
-    def __init__(): 
+    def __init__(self, A: list[tuple[float, float]], B: list[tuple[float, float]], W: int): 
         """
         Initialize latitude-longitude system to compare each animal's migratory path
         against its template path, i.e: [(lat1, lon1), (lat2, lon2), (lat3, lon3)]
@@ -10,4 +10,10 @@ class DTW:
         Latitude analogy -> horizontal stripes (e.x. rings of saturn)
         Longitude analogy -> vertical wedges (e.x. terry's chocolate orange)
         """
-        
+        self.individual_path: list[tuple[float, float]] = A #goes on the rows  
+        self.template_path: list[tuple[float, float]] = B #goes on the columns
+        self.ROWS, self.COLS = len(self.individual_rows), len(self.template_path)
+        self.window: int = W
+
+    def dynamic_time_warping(self) -> None: 
+        pass #temp
